2d5eaca3f61dbabd7e51052d2516607deed3afa3
captiontalha <captiontalha@outlook.com>
2020-12-28 15:38:25 +0500

    Add project files.

---
+++ KioskTestCoreApp/Controllers/PaymentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace KioskTestCoreApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public PaymentResponse Get(string Amount ,string UniqueId)
+        {
+            var serialPort = new SerialPort();
+            serialPort.PortName = "COM5";
+            serialPort.BaudRate = 115200;
+            serialPort.DataBits = 8;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();
+            }
+            serialPort.Write($"{Amount}");
+            //serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            Thread.Sleep(40000);
+            var b = 0;
+            var a = serialPort.ReadExisting();
+            var rng = new Random();
+            if (a.Contains("Response Code = 00"))
+            {
+                return new PaymentResponse
+                {
+                    IsSuccess=true,
+                    UniqueId= UniqueId
+                };
+            }
+            return new PaymentResponse
+            {
+                IsSuccess=false,
+                UniqueId= UniqueId
+            }
+            ;
+        }
+    }
+}
---
+++ KioskTestCoreApp/PaymentResponse.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace KioskTestCoreApp
+{
+    public class PaymentResponse
+    {
+        public bool IsSuccess { get; set; }
+
+        public string UniqueId { get; set; }
+
+    }
+}
